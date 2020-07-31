3a85e1e2127729641df300828c2c46765d7d338a
Chris Patterson <chris.patterson@canonical.com>
2020-07-31 15:54:49 -0400

    use gnome extension, use snapcraft-preloads, update skype
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ rm -f *.snap
 snapcraft snap
 sudo snap remove skype
 sudo snap install skype_*_amd64.snap --dangerous 
-for x in skype:bluez skype:browser-sandbox skype:camera skype:password-manager-service; do
+for x in skype:bluez skype:browser-sandbox skype:camera skype:password-manager-service skype:pulseaudio; do
 	sudo snap connect $x
 done
--- try.sh
+++ try.sh
@@ -3,6 +3,6 @@
 snapcraft try
 sudo snap try $(pwd)/prime
 
-for x in skype:bluez skype:browser-sandbox skype:camera skype:password-manager-service; do
+for x in skype:bluez skype:browser-sandbox skype:camera skype:password-manager-service skype:pulseaudio; do
 	sudo snap connect $x
 done
